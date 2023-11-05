cba790b5773f5f3c94be75b4adec7dcfffeeb37d
Michael Rasmussen <michaelrasmussen1337@gmail.com>
2023-11-05 11:22:14 +0100

    Calculate parameters in init routine

--- Sorgenti/c2pnew.asm
+++ Sorgenti/c2pnew.asm
@@ -25,10 +25,19 @@
 ;  A0 10099B78   A1 00000000   A2 1001E99C   A3 1000499C
 ;  A4 100AA0E4   A5 10089E96   A6 100A957C   A7 10099AE4
 
+; Mode (a1) bit 0: double x, bit 1: double y
 c2p8_init::
         move.l  a0,chunky
         move.l  d2,cwidth
         move.l  d3,cheight
+        move.l  #320,d0
+        move.l  #200,d1
+        sub.l   d2,d0
+        sub.l   d3,d1
+        lsr.l   #1,d0
+        lsr.l   #1,d1
+        move.l  d0,sxofs
+        move.l  d1,syofs
         rts
 
 ; void c2p8_go(register __a0 PLANEPTR *planes, // pointer to planes
@@ -36,13 +45,7 @@ c2p8_init::
 c2p8_go::
         movem.l d2-d3,-(sp)
         lea     -8(a0),a1              ; Move offset to bitmap
-        movem.l cwidth(pc),d0/d1/a0
-        move.w  #320,d2
-        move.w  #200,d3
-        sub.w   d0,d2
-        sub.w   d1,d3
-        lsr.w   #1,d2
-        lsr.w   #1,d3
+        movem.l cwidth(pc),d0-d3/a0
         bsr     c2p1x1_8_c5_bm_040
         movem.l (sp)+,d2-d3
         rts
@@ -50,7 +53,10 @@ c2p8_go::
 c2p8_waitblitter::
         rts
 
+        ; Keep in order of arguments for c2p routine
         cnop    0,4
 cwidth  ds.l    1
 cheight ds.l    1
+sxofs   ds.l    1
+syofs   ds.l    1
 chunky  ds.l    1
